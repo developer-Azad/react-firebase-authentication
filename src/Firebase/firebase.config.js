6c257bb42c7f4581d891d4546bf07128bed0914d
developer-Azad <akalamazad500@gmail.com>
2021-10-17 01:53:26 +0600

    first

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCplBMGtNa_E6BP2ocxVOTzGNMcfFanshw",
+    authDomain: "react-firebase-authentic-8a907.firebaseapp.com",
+    projectId: "react-firebase-authentic-8a907",
+    storageBucket: "react-firebase-authentic-8a907.appspot.com",
+    messagingSenderId: "455293192832",
+    appId: "1:455293192832:web:9fa1e7d36344ad62243465"
+  };
+  
+  export default firebaseConfig;
